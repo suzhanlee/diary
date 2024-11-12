a4a137b4782235232aa17d21d8c61883065e9483
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2024-11-12 18:03:52 +0900

    feat : diary 조회 기능 생성

--- diary/controller/controllers.py
+++ diary/controller/controllers.py
@@ -10,10 +10,12 @@ router = APIRouter()
 
 
 @router.post("/diary")
-def createDiary(createDiaryRq: CreateDiaryRq, db: Session = Depends(get_db)):
+def create_diary(createDiaryRq: CreateDiaryRq, db: Session = Depends(get_db)):
     member: Member = db.query(Member).filter(Member.id == createDiaryRq.user_id).first()
+
     if member is None:
         raise HTTPException(400, detail="존재하지 않는 사용자입니다.")
+
     diary: Diary = Diary(
         user_id=createDiaryRq.user_id,
         time=createDiaryRq.time,
@@ -28,3 +30,16 @@ def createDiary(createDiaryRq: CreateDiaryRq, db: Session = Depends(get_db)):
     db.refresh(diary)
 
     return {"diary_id": diary.id, "time": diary.time, "plan": diary.plan}
+
+
+@router.get("/diary/{user_id}")
+def get_diarites(user_id: int, db: Session = Depends(get_db)):
+    diaries: list[Diary] = db.query(Diary).filter(Diary.user_id == user_id).all()
+    if diaries.size() == 0:
+        return {"message": "사용자의 다이어리는 비어있습니다."}
+
+    return [
+        {"diary_id": diary_id, "time": time, "plan": plan}
+        for diary_id, time, plan in diaries
+    ]
+
